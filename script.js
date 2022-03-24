f4b3d53b1ff4b7d9597166808de1b14573841750
misakigrim <garmonweng@gmail.com>
2022-03-25 02:20:38 +1100

    mostly complete, finishing touches left

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let fullAnswer = ['h', 'a', 't', 'c', 'h']
-let keys = document.getElementsByClassName('key');
+let fullAnswer = ['t', 'r', 'u', 'm', 'p'] 
 
 const allRows = document.getElementsByClassName('game-row')
 const allTiles = document.getElementsByClassName('tile')
@@ -7,32 +6,54 @@ const allTiles = document.getElementsByClassName('tile')
 let tileCount = 0
 let rowCount = 0
 
+let keys = document.getElementsByClassName('key');
+
 for (let letter of keys) {
     let key = letter.textContent;
+
     letter.addEventListener('click', function() {
-        const row1tiles = allRows[rowCount].children
-        row1tiles[tileCount].textContent += key
-        tileCount += 1
-        if (tileCount == 5){
-            tileCount = 0;
-            rowCount += 1;
+        const rowTiles = allRows[rowCount].children;
+
+        if (key == '␡' || key == '␡' && tileCount == 5) {
+            tileCount--;
+            rowTiles[tileCount].textContent = "";
+        } else if (key == '␡' && tileCount == 0) {
+            tileCount = 0
+        } else if (tileCount < 5) {
+            rowTiles[tileCount].textContent += key
+            rowTiles[tileCount].classList.add('filled');
+            tileCount ++;
         } 
-    })
     
+        if (tileCount == 5) {
+                tileCount = 0;
+                rowCount ++;
+        }
+
+    
+    // switch (key) {
+    //     case '␡':
+    //         rowTiles[tileCount].textContent = rowTiles[tileCount].textContent.slice(0, rowTiles[tileCount].textContent.length-1);
+    //         tileCount--
+    //     default:
+    //         // output.textContent += key;
+        // }
+    })
 }
+
 
 
 function test(rowId) {
     const row = document.getElementById(rowId).children
     for (const index in fullAnswer) {
-        if (fullAnswer[index] === row[index].textContent) {
+       if (fullAnswer[index] === row[index].textContent) {
             row[index].classList.add('correct');
-            continue;
-        } else if (fullAnswer[index] == row[index].textContent){
-            row[index].classList.add('correct')
-            console.log(fullAnswer[index], row[index].textContent)
         } else if (fullAnswer.includes(row[index].textContent)) {
             row[index].classList.add('positional')
+        // } else if ((fullAnswer[index] == row[index].textContent) && 
+        //             (row[index].classList.contains('positional').textContent == 
+        //             row[index].classList.contains('correct').textContent)){
+        //     row[index].classList.remove('positional')
         } else if (fullAnswer[index] !== row[index].textContent) {
             const unused = row[index].textContent
             for (const index in keys) {
@@ -41,7 +62,10 @@ function test(rowId) {
                 }
             }
         }
-    }   
+    }  
+    if (fullAnswer === row.textContent) {
+        // explode();
+    }  
 }
 
 const submit = document.getElementById('enter')
@@ -51,4 +75,30 @@ submit.addEventListener('click', function() {
     test('row3');
     test('row4');
     test('row5');
+    answerCheck('row1');
+    answerCheck('row2');
+    answerCheck('row3');
+    answerCheck('row4');
+    answerCheck('row5');
 })
+
+function answerCheck(rowId) {
+    const row = document.getElementById(rowId).children;
+    if ((row[0].textContent === fullAnswer[0]) && 
+        (row[1].textContent === fullAnswer[1]) && 
+        (row[2].textContent === fullAnswer[2]) && 
+        (row[3].textContent === fullAnswer[3]) && 
+        (row[4].textContent === fullAnswer[4])) {
+            row[0].classList.add('superCorrect', 'tile2');
+            row[1].classList.add('superCorrect', 'tile2');
+            row[2].classList.add('superCorrect', 'tile2');
+            row[3].classList.add('superCorrect', 'tile2');
+            row[4].classList.add('superCorrect', 'tile2');
+            setTimeout(openLink(), 5000);
+    }
+}
+
+function openLink() {
+    setTimeout(function(){ window.open("videopage.html", "_blank").focus(); }, 3500);
+}
+
